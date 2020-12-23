483c86b8a82aa2c44d15d375d9986efc30fa8118
akira-yp <yog.puripuri@gmail.com>
2020-12-24 01:02:54 +0900

    一覧表示、投稿、編集、更新、削除機能を実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  before_action :get_all_posts, only:[:index,:create,:edit,:update]
+  before_action :set_post, only:[:edit,:update,:destroy]
+  def index
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice:"メッセージを投稿しました！"
+    else
+      render :index
+    end
+  end
+  def edit
+  end
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice:"メッセージを更新しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"メッセージを削除しました！"
+  end
+  def confirm
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content,:created_at)
+  end
+  def get_all_posts
+    @posts = Post.all
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
