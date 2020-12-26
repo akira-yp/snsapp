a032856d9f694792972d27819599e9de6ebd9ea8
akira-yp <yog.puripuri@gmail.com>
2020-12-26 22:58:28 +0900

    残り文字数のカウント機能を実装

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,20 @@
 $(function(){
-    $(document).on('keyup','#post_content',function(){
+  $(document).on('keyup','#post_content',function(){
     let count = $(this).val().length;
     let $submit = $('.submit_btn');
-    $('.count_letters').text(count);
     if( count >= 1 && count <= 140 ){
       $submit.css('background-color','#1fa2f2');
       $submit.removeClass('no_click');
     } else {
       $submit.css('background-color','#9bd1f9');
       $submit.addClass('no_click');
+    };
+    let calc_letters = 140 - count ;
+    $('.count_letters').text(`残り${calc_letters}文字`);
+    if(count >= 140){
+      $('.count_letters').css('color','#e0255e');
+    }else{
+      $('.count_letters').css('color','#999999');
     };
   });
 });
